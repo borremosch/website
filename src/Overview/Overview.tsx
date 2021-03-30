099c299266878aa0737a12c5c0a8ed6b4747a7c7
Borre <borre.mosch@cubonacci.com>
2021-03-30 23:04:57 +0200

    Updated overview page

--- src/Overview/Overview.tsx
+++ src/Overview/Overview.tsx
@@ -1,15 +1,20 @@
 import styles from "./Overview.module.css";
 import firefliesFeatureGraphic from "../Images/feature-graphic-fireflies.jpg";
 import mathsAppFeatureGraphic from "../Images/feature-graphic-mathsapp.jpg";
+import { Link } from "react-router-dom";
 
 export function Overview() {
   return (
     <div className={styles["overview"]}>
       <div className={styles["app"]}>
-        <img src={firefliesFeatureGraphic} alt="Fireflies" />
+        <Link to="/Fireflies">
+          <img src={firefliesFeatureGraphic} alt="Fireflies" />
+        </Link>
       </div>
       <div className={styles["app"]}>
-        <img src={mathsAppFeatureGraphic} alt="MathsApp" />
+        <Link to="/MathsApp/">
+          <img src={mathsAppFeatureGraphic} alt="MathsApp" />
+        </Link>
       </div>
     </div>
   );
