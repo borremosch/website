3a3443a41ce739e16f2eff723f35a852e51b949b
Borre <borre.mosch@cubonacci.com>
2021-04-01 20:05:18 +0200

    Overview is now responsive

--- src/Overview/Overview.tsx
+++ src/Overview/Overview.tsx
@@ -1,17 +1,17 @@
-import styles from "./Overview.module.css";
-import firefliesFeatureGraphic from "../Images/feature-graphic-fireflies.jpg";
-import mathsAppFeatureGraphic from "../Images/feature-graphic-mathsapp.jpg";
-import { Link } from "react-router-dom";
+import styles from './Overview.module.css';
+import firefliesFeatureGraphic from '../Images/feature-graphic-fireflies.jpg';
+import mathsAppFeatureGraphic from '../Images/feature-graphic-mathsapp.jpg';
+import { Link } from 'react-router-dom';
 
 export function Overview() {
   return (
-    <div className={styles["overview"]}>
-      <div className={styles["app"]}>
+    <div className={styles['overview']}>
+      <div className={styles['app']}>
         <Link to="/Fireflies">
           <img src={firefliesFeatureGraphic} alt="Fireflies" />
         </Link>
       </div>
-      <div className={styles["app"]}>
+      <div className={styles['app']}>
         <Link to="/MathsApp/">
           <img src={mathsAppFeatureGraphic} alt="MathsApp" />
         </Link>
